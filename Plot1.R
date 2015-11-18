1be326fad23306a2bbf7b5dfa49bef5b28f4c542
elcovi <2coviella@gmail.com>
2015-11-17 21:30:56 -0500

    ExData_Plotting1
    Exploratory Data Analysis - Project 1

--- Plot1.R
+++ Plot1.R
@@ -4,16 +4,27 @@
 library(dplyr)
 library(readr)
 
-## Get data into data frame:
-houseData <- read_csv2("household_power_consumption.txt", col_names =  TRUE, na = "?")
-## str(houseData)
+## Get data into data frame, using Hadley Wickham "readr" package, which allows for reading ";" separted data and type assignement to the data in one step:
+
+houseData <- read_csv2("household_power_consumption.txt", 
+                       col_names = TRUE, 
+                       col_types = "ccnnnnnnn", 
+                       na = "?")
+str(houseData)
 
 ## Filter a subset of the data for dates 1/2/2007 & 2/2/2007:
 subHouseData <- houseData[houseData$Date %in% c("1/2/2007","2/2/2007"), ]
+
 ## str(subHouseData)
 
 ## Make the file and draw the graph:
-png("plot1.png", width=480, height=480)
-hist(subHouseData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+png("plot1.png", 
+    width=480, 
+    height=480)
+hist(subHouseData$Global_active_power, 
+     col="red", 
+     main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)")
 ## Turn off graphic device:
 dev.off()
+
--- Plot2.R
+++ Plot2.R
@@ -4,19 +4,35 @@
 library(dplyr)
 library(readr)
 
-## Get data into data frame:
-houseData <- read_csv2("household_power_consumption.txt", col_names =  TRUE, na = "?")
+## Get data into data frame, using Hadley Wickham "readr" package, which allows for reading ";" separted data and type assignement to the data in one step:
+
+houseData <- read_csv2("household_power_consumption.txt", 
+                       col_names =  TRUE, 
+                       col_types = "ccnnnnnnn", 
+                       na = "?")
 ## str(houseData)
 
 ## Filter a subset of the data for dates 1/2/2007 & 2/2/2007:
 subHouseData <- houseData[houseData$Date %in% c("1/2/2007","2/2/2007"), ]
+
 ## str(subHouseData)
 
-dateFormat <- strptime(paste(subHouseData$Date, subHouseData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+dateFormat <- strptime(paste(subHouseData$Date, 
+                             subHouseData$Time, sep=" "), 
+                       "%d/%m/%Y %H:%M:%S") 
 
 ## Make the file and draw the graph:
-png("plot2.png", width=480, height=480)
-plot(dateFormat, subHouseData$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+png("plot2.png", 
+    width=480, 
+    height=480)
+plot(dateFormat, 
+     subHouseData$Global_active_power, 
+     type="l", 
+     xlab="", 
+     ylab="Global Active Power (kilowatts)")
 
 ## Turn off graphic device:
 dev.off()
+
+
+
